95ecc17d34c9c0b8e35a3f64ca473789e7d9dd8b
Sarah_RZ <rezaeisara1994@yahoo.com>
2023-08-22 15:20:08 +0330

    first commit

---
+++ ex_1.py
@@ -0,0 +1,12 @@
+# Write code to extract the number at the end of the line below. 
+# Convert the extracted value to a floating point number and print it out.
+
+text = "Lorem ipsum dolor sit amet elit, consectetur adipiscing elit    20.65434"
+
+ftext = text.find("adipiscing")
+find_text = text.find(' ', ftext)
+part_text = text[find_text + 5 : ]
+strip_text = part_text.lstrip()
+float_text = float(strip_text)
+
+print(float_text)
